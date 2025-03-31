551144adcd432a9d9056815cfd2f78fde8887a2e
Hashashin-Company <Hashashincompany@gmail.com>
2025-03-31 14:18:12 +0330

    upload anime project

---
+++ js/index.js
@@ -0,0 +1,5 @@
+let pageup_btn = document.getElementById('pageup');
+function scrollfunction() {
+    document.body.scrollTop = 0; //chrome
+    document.documentElement.scrollTop = 0; //edge , firefox 
+}
